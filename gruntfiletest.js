fffc66215fc0c456820a4fd6df928c59bf1bd007
CharlotteMoriarty <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-10-27 22:01:18 +0200

    Update gruntfiletest.js

--- gruntfiletest.js
+++ gruntfiletest.js
@@ -11,37 +11,13 @@ module.exports = function (grunt) {
                 }
             }
         },
-        watch: {
-            scripts: {
-                files: ['sass/*.sass'],
-                tasks: ['sass'],
-                options: {
-                spawn: false,
-                },
-            }
-        },
-        browserSync: {
-            default_options: {
-                bsFiles: {
-                    src: [
-                        "css/*.css",
-                        "*.html"
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    server: {
-                        baseDir: "./"
-                    }
-                }
-            },
         jshint: {
-            all: ['js/*.js']
-    }
+            all: 
+            ['js/*.js'] 
+            
+        }
     });
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-jshint');    
-    grunt.registerTask('default', ['sass','browserSync','watch','jshint']);
+    grunt.registerTask('default', ['sass','jshint']);
 };
