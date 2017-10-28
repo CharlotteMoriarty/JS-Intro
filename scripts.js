68d516c37493faab73b57c4c0330890dad17ec50
CharlotteMoriarty <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-10-28 14:58:06 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-var halloween = prompt('Trick or treat');
-alert('You selected '+ halloween);
-console.log('Good choice');
+//ale o co chodzi
+var stranger = prompt('up or down');
+alert('You are going ' + stranger);
+console.log('');
+//komentarz 1
+//komentarz 2
